c4787625e1f7e79b8d54bb911b57093c02c951d6
MarcosCervela <101353178+MarcosCervela@users.noreply.github.com>
2022-10-25 21:28:07 +0200

    cambios completos

--- pages/interfaz3.js
+++ pages/interfaz3.js
@@ -18,3 +18,58 @@ function handleDrop(event) {
     var data = event.dataTransfer.getData("text");
     event.target.appendChild(document.getElementById(data));
 }
+
+
+
+//Creaccion de subelemento
+
+
+
+const input = document.querySelector("input");
+const addBtn = document.querySelector(".btn-add");
+const ul = document.getElementById("listbox3")
+const empty = document.querySelector(".empty");
+
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const text = input.value;
+
+  if (text !== "") {
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    p.textContent = text;
+
+    li.appendChild(p);
+    li.appendChild(addDeleteBtn());
+    ul.appendChild(li);
+
+    input.value = ""; // Para que el valor una vez le des al boton de añadir vuelva a estar vacio el imput
+    empty.style.display = "none"; //Ponemos le display a 0
+  }
+});
+
+function addDeleteBtn() {
+  const deleteBtn = document.createElement("button");
+
+  deleteBtn.textContent = "X";
+  deleteBtn.className = "btn-delete";
+
+  deleteBtn.addEventListener("click", (e) => {
+    const item = e.target.parentElement;
+    ul.removeChild(item);
+
+    const items = document.querySelectorAll("li");
+
+    if (items.length === 0) {
+      empty.style.display = "block";
+    }
+  });
+
+  return deleteBtn;
+}
+
+    
+
+
