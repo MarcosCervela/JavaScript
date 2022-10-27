c2c86283959bac80d68cd4b8cf28c9a45607e8bb
MarcosCervela <101353178+MarcosCervela@users.noreply.github.com>
2022-10-27 19:47:52 +0200

    MeotodsDinemicos

--- pages/interfaz3.js
+++ pages/interfaz3.js
@@ -1,4 +1,4 @@
-const COLUMN_CLASS = 'columna'
+/*const COLUMN_CLASS = 'columna'
 
 
 function allowDrop(event) {
@@ -18,14 +18,161 @@ function handleDrop(event) {
     var data = event.dataTransfer.getData("text");
     event.target.appendChild(document.getElementById(data));
 }
+ondrop="handleDrop(event)" ondragover="allowDrop(event)"
+*/
+
+
+//Drag and Drop
+
+/*
+const fill = document.querySelectorAll('.cardbody');
+const empties = document.querySelectorAll('.columna');
+
+
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+
+for (const empty of empties) {
+  empty.addEventListener('dragover', dragOver);
+  empty.addEventListener('dragenter', dragEnter);
+  empty.addEventListener('dragleave', dragLeave);
+  empty.addEventListener('drop', dragDrop);
+}
 
 
 
-//Creación de tarjeta
-const title2 = document.getElementById("recipient-title");
-const description2 = document.getElementById("recipient-descrip");
-const addBtn2 = document.querySelector(".btn-add");
-const cardContainer = document.getElementById("card-container2");
+function dragStart() {
+  this.className += ' hold';
+  setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd() {
+  this.className = 'fill';
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += ' hovered';
+}
+
+function dragLeave() {
+  this.className = 'empty';
+}
+
+function dragDrop() {
+  this.className = 'empty';
+  this.append(fill);
+}
+
+
+*/
+
+
+//Creación de tarjetabox1
+
+const title1 = document.getElementById("recipient-title1");
+const description1 = document.getElementById("recipient-descrip1");
+const addBtn1 = document.getElementById("btnAddCard1");
+const cardContainer1 = document.getElementById("card-container1");
+const empty1 = document.querySelector(".empty");
+
+
+
+
+addBtn1.addEventListener("click", (e) =>{
+    e.preventDefault();
+    //var UID = "id-" + Math.floor(Math.random() * 999999);
+    
+    const titlecard1 = title1.value;
+    const discripcard1 = description1.value;
+
+    //Generador de IDs
+    function generateUUID() {
+      var d = new Date().getTime();
+      var uuid = 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+          var r = (d + Math.random() * 16) % 16 | 0;
+          d = Math.floor(d / 16);
+          return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+      });
+      return uuid;
+  }
+    
+    
+    
+
+    if(titlecard1 !== "" && discripcard1 !== ""){
+      
+        const card = document.createElement("div");
+        const p = document.createElement("h3");
+        const p1 = document.createElement("p");
+        const deleteBtn = document.createElement("button");
+        const modifBtn = document.createElement("button");
+
+        
+        p.textContent = titlecard1;
+        p1.textContent = discripcard1;
+
+        deleteBtn.textContent = "Eliminar";
+        deleteBtn.className = "btn btn-danger";
+        deleteBtn.style = "";
+
+        modifBtn.textContent = "Modificar"
+        modifBtn.className = "btn btn-warning"
+
+        
+        card.appendChild(p);
+        card.appendChild(p1);
+        card.appendChild(deleteBtn);
+        card.appendChild(modifBtn);
+        
+        
+        
+
+        card.className += "container-fluid cardbody";
+        card.style +="width: 95%; height: 150px;margin: 8px;padding: 5px;background-color: rgb(235, 230, 230);"
+        card.id += generateUUID();
+        card.draggable += "true";
+        
+        
+        cardContainer1.appendChild(card);
+
+
+
+        deleteBtn.addEventListener("click", (e) => {
+
+            const item = e.target.parentElement;
+            cardContainer1.removeChild(item);
+
+            
+
+
+
+        })
+        
+        
+        title1.value = ""; // Para que el valor una vez le des al boton de añadir vuelva a estar vacio el imput
+        description1.value ="";
+        empty1.style.display = "none"; //Ponemos le display a 0
+        
+
+      
+
+      }
+    
+});
+
+
+
+//Creación de tarjetabox2
+const title2 = document.getElementById("recipient-title2");
+const description2 = document.getElementById("recipient-descrip2");
+const addBtn2 = document.getElementById("btnAddCard2");
+const cardContainer2 = document.getElementById("card-container2");
 const empty = document.querySelector(".empty");
 
 
@@ -37,22 +184,63 @@ addBtn2.addEventListener("click", (e) =>{
     const titlecard2 = title2.value;
     const discripcard2 = description2.value;
 
-    debugger
+    //Generador de IDs
+    function generateUUID() {
+      var d = new Date().getTime();
+      var uuid = 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+          var r = (d + Math.random() * 16) % 16 | 0;
+          d = Math.floor(d / 16);
+          return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+      });
+      return uuid;
+  }
 
     if(titlecard2 !== "" && discripcard2 !== ""){
 
         const card = document.createElement("div");
-        const p = document.createElement("p");
+        const p = document.createElement("h3");
         const p1 = document.createElement("p");
-
+        const deleteBtn = document.createElement("button");
+        const modifBtn = document.createElement("button");
 
         p.textContent = titlecard2;
         p1.textContent = discripcard2;
 
+        deleteBtn.textContent ="Eliminar";
+        deleteBtn.className = "btn btn-danger";
+        deleteBtn.style = "";
 
+        modifBtn.textContent = "Modificar"
+        modifBtn.className = "btn btn-warning"
+
+        
         card.appendChild(p);
         card.appendChild(p1);
-        cardContainer.appendChild(card);
+        card.appendChild(deleteBtn);
+        card.appendChild(modifBtn);
+        
+        
+
+        card.className += "container-fluid cardbody";
+        card.style +="width: 95%; height: 150px;margin: 8px;padding: 5px;background-color: rgb(235, 230, 230);";
+        card.id += generateUUID();
+        card.draggable += "true";
+        
+        
+        cardContainer2.appendChild(card);
+
+
+
+        deleteBtn.addEventListener("click", (e) => {
+
+            const item = e.target.parentElement;
+            cardContainer2.removeChild(item);
+
+            
+
+
+
+        })
         
         
         title2.value = ""; // Para que el valor una vez le des al boton de añadir vuelva a estar vacio el imput
@@ -62,51 +250,8 @@ addBtn2.addEventListener("click", (e) =>{
 
 
 
-    }
+      }
 
-
-
-
-
-})
-
-
-
-//Creaccion de subelemento
-
-/*
- 
-const title3 = document.getElementById("recipient-title");
-const description3 = document.getElementById("recipient-descrip");
-const addBtn = document.querySelector(".btn-add");
-const ul = document.getElementById("listbox3")
-const empty = document.querySelector(".empty");
-
-
-addBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  const text1 = title3.value;
-  const text2 =  description3.value;
-
-  if (text1 !== "" && text2!== "") {
-    const li = document.createElement("li");
-    const p = document.createElement("p");
-    const li1 = document.createElement("li");
-    const p1 = document.createElement("p");
-    p.textContent = text1;
-    p1.textContent = text2;
-
-    li.appendChild(p);
-    li.appendChild(p1);
-    li.appendChild(addDeleteBtn());
-    ul.appendChild(li);
-
-    title3.value = ""; // Para que el valor una vez le des al boton de añadir vuelva a estar vacio el imput
-    description3.value ="";
-    empty.style.display = "none"; //Ponemos le display a 0
-    
-  }
 });
 
 
@@ -114,26 +259,93 @@ addBtn.addEventListener("click", (e) => {
 
 
 
-function addDeleteBtn() {
-  const deleteBtn = document.createElement("button");
+//CreacionSubelementosBox3
 
-  deleteBtn.textContent = "X";
-  deleteBtn.className = "btn-delete";
 
-  deleteBtn.addEventListener("click", (e) => {
-    const item = e.target.parentElement;
-    ul.removeChild(item);
+const title3 = document.getElementById("recipient-title3");
+const description3 = document.getElementById("recipient-descrip3");
+const addBtn3 = document.getElementById("btnAddCard3");
+const cardContainer3 = document.getElementById("card-container3");
+const empty3 = document.querySelector(".empty");
 
-    const items = document.querySelectorAll("li");
 
-    if (items.length === 0) {
-      empty.style.display = "block";
-    }
-  });
 
-  return deleteBtn;
-}
 
-    */
+addBtn3.addEventListener("click", (e) =>{
+    e.preventDefault();
 
+    const titlecard3 = title3.value;
+    const discripcard3 = description3.value;
+
+    //Generador de IDs
+    function generateUUID() {
+      var d = new Date().getTime();
+      var uuid = 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+          var r = (d + Math.random() * 16) % 16 | 0;
+          d = Math.floor(d / 16);
+          return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+      });
+      return uuid;
+  }
+
+    if(titlecard3 !== "" && discripcard3 !== ""){
+
+        const card = document.createElement("div");
+        const p = document.createElement("h3");
+        const p1 = document.createElement("p");
+        const deleteBtn = document.createElement("button");
+        const modifBtn = document.createElement("button");
+
+
+        p.textContent = titlecard3;
+        p1.textContent = discripcard3;
+
+        //Boton Eliminar
+        deleteBtn.textContent ="Eliminar";
+        deleteBtn.className = "btn btn-danger btn_eliminar";
+        deleteBtn.style = "";
+
+        //Boton Modificar
+        modifBtn.textContent = "Modificar"
+        modifBtn.className = "btn btn-warning"
+
+        
+        card.appendChild(p);
+        card.appendChild(p1);
+        card.appendChild(deleteBtn);
+        card.appendChild(modifBtn);
+        
+
+        card.className += "container-fluid cardbody";
+        card.style +="width: 95%; height: 150px;margin: 8px;padding: 5px;background-color: rgb(235, 230, 230);";
+        card.id += generateUUID();
+        card.draggable += "true";
+        
+        
+        cardContainer3.appendChild(card);
+
+
+
+        deleteBtn.addEventListener("click", (e) => {
+
+            const item = e.target.parentElement;
+            cardContainer3.removeChild(item);
+
+            
+
+
+
+        })
+        
+        
+        title3.value = ""; // Para que el valor una vez le des al boton de añadir vuelva a estar vacio el imput
+        description3.value ="";
+        empty3.style.display = "none"; //Ponemos le display a 0
+        
+
+
+
+      }
+
+});
 
